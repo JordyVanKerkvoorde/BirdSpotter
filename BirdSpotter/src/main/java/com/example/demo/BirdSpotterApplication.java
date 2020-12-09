58cd25b198c12ef04cc8e7a3bb0a37119c47e483
Jordy <jordy.vankerkvoorde@student.hogent.be>
2020-12-09 01:00:23 +0100

    Remove unused imports

--- BirdSpotter/src/main/java/com/example/demo/BirdSpotterApplication.java
+++ BirdSpotter/src/main/java/com/example/demo/BirdSpotterApplication.java
@@ -1,16 +1,7 @@
 package com.example.demo;
 
-import java.util.Collections;
-
-import javax.servlet.Servlet;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.SessionCookieConfig;
-import javax.servlet.SessionTrackingMode;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 
 import service.SpottedBirdService;
